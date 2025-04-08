fd863a5b6ab7a8446f01c6dde754cc847c9e3b8d
Aditya Nikam <aditya.nikam.s73@kalvium.community>
2025-04-08 14:32:58 +0530

    Bug fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import HomePage from './components/Home';
+import HomePage from './pages/Home';
 import AuthPage from './components/auth';
 
 
