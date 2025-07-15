d185e0077f637cfcdb379b0afd6c310fd4920005
Aditya Nikam <aditya.nikam.s73@kalvium.community>
2025-07-15 12:16:44 +0530

    schema created

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import mongoose from "mongoose"
+
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+const PORT = process.env.PORT || 3000
+// const URL = process.env.URL
+
+// mongoose.connect(URL)
+//   .then(() => {
+//     console.log("Database Connected!");
+//     startServer();
+//   })
+//   .catch((err) => {
+//     console.error("Database Connection Failed:", err.message);
+//     process.exit(1);
+//   });
+
+
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hello from TriMedi Backend!' });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
